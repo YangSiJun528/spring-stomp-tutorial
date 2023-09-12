aab07d3448a6e2e50e007c83d89bc47a4e6d4f22
YangSiJun528 <yangsijun5528@gmail.com>
2023-09-13 08:47:18 +0900

    api 정의

--- src/main/kotlin/dev/yangsijun/stomptutorial/room/controller/RoomRestController.kt
+++ src/main/kotlin/dev/yangsijun/stomptutorial/room/controller/RoomRestController.kt
@@ -1,10 +1,14 @@
 package dev.yangsijun.stomptutorial.room.controller
+// package com.gsmNetworking.chat
 
 import dev.yangsijun.stomptutorial.message.dto.websocket.ChatMessage
+import dev.yangsijun.stomptutorial.room.domain.Room
 import org.springframework.messaging.handler.annotation.DestinationVariable
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
@@ -12,6 +16,15 @@ import java.util.UUID
 @RequestMapping("/api/room/v1")
 class RoomRestController {
 
-
+    @MessageMapping("/by-user-id/{userId}")
+    fun getRooms(
+        @PathVariable("userId") userId: UUID,
+        @RequestParam(name = "page") page: Int,
+        @RequestParam(name = "size") size: Int
+    ): List<Room> {
+        // TODO 메서드 이름 생각해보기
+        //  최신 채팅 방 n개 가져오는 역할임
+        return listOf()
+    }
 
 }
