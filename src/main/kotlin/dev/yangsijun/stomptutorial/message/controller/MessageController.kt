8d4310238ed7af65f3eabebee68eba63f8031c58
YangSiJun528 <yangsijun5528@gmail.com>
2023-09-12 15:14:45 +0900

    controller 생성

---
+++ src/main/kotlin/dev/yangsijun/stomptutorial/message/controller/MessageController.kt
@@ -0,0 +1,9 @@
+package dev.yangsijun.stomptutorial.message.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/message/v1")
+class MessageController {
+}
