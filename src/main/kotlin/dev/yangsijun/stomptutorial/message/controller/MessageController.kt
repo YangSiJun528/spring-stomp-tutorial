289b00911803218ba1898591af794d2a7df4f12f
YangSiJun528 <yangsijun5528@gmail.com>
2023-09-12 15:16:11 +0900

    주석 추가

--- src/main/kotlin/dev/yangsijun/stomptutorial/message/controller/MessageController.kt
+++ src/main/kotlin/dev/yangsijun/stomptutorial/message/controller/MessageController.kt
@@ -6,4 +6,6 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/message/v1")
 class MessageController {
+    // TODO 여기는 뭐 딱히 없는듯?
+    //  뭔가 패키지 구조가 잘못된 거 같기도 함, 채팅, 메시지 하나로 합친게 더 낫지 않나?
 }
