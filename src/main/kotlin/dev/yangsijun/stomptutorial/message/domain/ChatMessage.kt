b79b84377598ebcae760455e9314d08944811032
YangSiJun528 <yangsijun5528@gmail.com>
2023-09-12 14:39:18 +0900

    static factory method 적용 & 인덱스 적용

--- src/main/kotlin/dev/yangsijun/stomptutorial/message/domain/ChatMessage.kt
+++ src/main/kotlin/dev/yangsijun/stomptutorial/message/domain/ChatMessage.kt
@@ -2,13 +2,28 @@ package dev.yangsijun.stomptutorial.message.domain
 
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.CompoundIndex
 import org.springframework.data.mongodb.core.mapping.Document
 import java.util.UUID
 
 @Document(collection = "chat")
-class ChatMessage(
+// 방 id(=), 채팅 id(=,<>)
+// - 대부분 최근 채팅을 보기 때문에 최신순 정렬, 사실 방은 (=) 조건이라 냅둠
+@CompoundIndex(def = "{'roomId': 1, 'id': -1}")
+class ChatMessage private constructor(
     @Id
     val id: ObjectId,
     val roomId: UUID,
     val textContent: String
-)
+) {
+    companion object {
+        // DB 저장하기 직전에 사용 - ObjectId에 시간 값이 저장되기 때문
+        fun create(roomId: UUID, textContent: String): ChatMessage {
+            return ChatMessage(id = ObjectId.get(), roomId = roomId, textContent = textContent)
+        }
+
+        fun update(id: ObjectId, roomId: UUID, textContent: String): ChatMessage {
+            return ChatMessage(id = id, roomId = roomId, textContent = textContent)
+        }
+    }
+}
--- src/main/kotlin/dev/yangsijun/stomptutorial/room/domain/ChatRoom.kt
+++ src/main/kotlin/dev/yangsijun/stomptutorial/room/domain/ChatRoom.kt
@@ -1,12 +1,26 @@
 package dev.yangsijun.stomptutorial.room.domain
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.CompoundIndex
 import org.springframework.data.mongodb.core.mapping.Document
 import java.util.*
 
 @Document(collection = "room")
+// 특정 유저(=), 유저의 마지막으로 확인한 채팅(=,<>)
+// - 대부분 최근 방을 보기 때문에 최신순 정렬, 유저id는 범위 조건을 사용하지 않기 때문에 걍 냅둠
+@CompoundIndex(def = "{'userInfos.userId': 1, 'userInfos.lastViewedChatMessage': -1}")
 class ChatRoom(
     @Id
     val id: UUID,
     val userInfos: List<UserInfo>
-)
+) {
+    companion object {
+        fun create(userInfos: List<UserInfo>): ChatRoom {
+            return ChatRoom(id = UUID.randomUUID(), userInfos = userInfos)
+        }
+
+        fun update(id: UUID, userInfos: List<UserInfo>): ChatRoom {
+            return ChatRoom(id = UUID.randomUUID(), userInfos = userInfos)
+        }
+    }
+}
