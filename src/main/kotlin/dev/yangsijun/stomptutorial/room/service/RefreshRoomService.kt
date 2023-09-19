76c21d895d625cebc68e1bf78fa30cea5d2dfdd7
YangSiJun528 <yangsijun5528@gmail.com>
2023-09-19 14:58:23 +0900

    Update RefreshRoomService.kt

--- src/main/kotlin/dev/yangsijun/stomptutorial/room/service/RefreshRoomService.kt
+++ src/main/kotlin/dev/yangsijun/stomptutorial/room/service/RefreshRoomService.kt
@@ -4,6 +4,7 @@ import dev.yangsijun.stomptutorial.chat.domain.BaseChat
 import dev.yangsijun.stomptutorial.chat.repository.ChatRepository
 import dev.yangsijun.stomptutorial.room.domain.Room
 import dev.yangsijun.stomptutorial.room.domain.UserInfo
+import dev.yangsijun.stomptutorial.room.dto.common.RoomInfoDto
 import dev.yangsijun.stomptutorial.room.message.res.RoomInfoMessage
 import org.bson.types.ObjectId
 import org.springframework.stereotype.Service
@@ -16,7 +17,7 @@ class RefreshRoomService(
     private val chatRepository: ChatRepository
 ) {
 
-    fun execute(room: Room, newChat: BaseChat): Map<UUID, RoomInfoMessage> {
+    fun execute(room: Room, newChat: BaseChat): List<RoomInfoDto> {
         val chats: List<BaseChat> = chatRepository.findAllByRoomIdOrderByIdDesc(roomId = room.id)
 
         val roomInfos = RoomInfoManager.execute(room, newChat, chats)
